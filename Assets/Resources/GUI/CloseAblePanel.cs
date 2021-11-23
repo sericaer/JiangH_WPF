d5475a0fdad96d305ae14ab7f15e73f821f9ae78
sericaer <sericaer@outlook.com>
2021-11-23 20:54:36 +0800

    Add Native and JiangH.UI

---
+++ Assets/Resources/GUI/CloseAblePanel.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+namespace JiangH
+{
+
+    [TemplatePart(Name = "CloseButton", Type = typeof(Button))]
+    public class CloseAblePanel : ContentControl
+    {
+        static CloseAblePanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CloseAblePanel), new FrameworkPropertyMetadata(typeof(CloseAblePanel)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            ApplyTemplate();
+
+            var partControlsLayer = GetTemplateChild("CloseButton") as Button;
+            partControlsLayer.Click += (sender, e) =>
+            {
+                var parent = this.Parent as Panel;
+                parent.Children.Remove(this);
+            };
+        }
+    }
+}
--- Assets/Scenes/MainScene/JiangHMainView.xaml.cs
+++ Assets/Scenes/MainScene/JiangHMainView.xaml.cs
@@ -1,9 +1,15 @@
 #if UNITY_5_3_OR_NEWER
 #define NOESIS
 using Noesis;
+using System.IO;
 #else
 using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
 #endif
 
 namespace JiangH
@@ -16,13 +22,38 @@ namespace JiangH
         public JiangHMainView()
         {
             InitializeComponent();
+
+#if NOESIS
+            var filePath = UnityEngine.Application.streamingAssetsPath;
+#else
+            var filePath = "../../../Assets/StreamingAssets";
+#endif
+            var str = File.ReadAllText(filePath + "/mods/native/SceneMain.xaml");
+
+            var userControl = XamlReader.Parse(str) as UserControl;
+
+            var uiElement = userControl.Content as UIElement;
+            userControl.Content = null;
+
+            this.rootCanvas.Children.Add(uiElement);
         }
 
 #if NOESIS
         private void InitializeComponent()
         {
             NoesisUnity.LoadComponent(this);
+
+            var dict = (ResourceDictionary)GUI.LoadXaml("Assets/Resources/GUI/Generic.xaml");
+
+            ResourceDictionary app = Noesis.GUI.GetApplicationResources();
+            app.MergedDictionaries.Add(dict);
+
+            rootCanvas = (Canvas)FindName("rootCanvas");
         }
+
+        private Canvas rootCanvas;
 #endif
+
+
     }
 }
