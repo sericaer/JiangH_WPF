668ecfc1b4291619152f6b41e78f5900040c4140
sericaer <sericaer@outlook.com>
2021-11-26 18:38:49 +0800

    Add PlayerButton

--- Assets/Resources/GUI/ImageButton.cs
+++ Assets/Resources/GUI/ImageButton.cs
@@ -5,6 +5,7 @@ using System.IO;
 using UnityEngine;
 #else
 using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -15,7 +16,7 @@ namespace JiangH
 {
     public class ImageButton : Button
     {
-        public const string defaultImageName = "BUTTON_IMAGE_DEFAULT";
+        public const string defaultImageName = "BUTTON_IMAGE_DEFAULT.jpg";
 
         static ImageButton()
         {
@@ -26,7 +27,7 @@ namespace JiangH
         {
             get
             {
-                var imageFileName = Name == null || Name == "" ? defaultImageName : $"BUTTON_IMAGE_{Name}";
+                var imageFileName = Name == null || Name == "" ? defaultImageName : $"BUTTON_IMAGE_{Name}.jpg";
 
                 return MakeImageBrush(imageFileName);
 
@@ -35,13 +36,18 @@ namespace JiangH
 
         private Brush MakeImageBrush(string imageFileName)
         {
+            var path = $"D:\\git\\NoesisStudy\\Assets\\StreamingAssets\\{imageFileName}";
+            if(!File.Exists(path))
+            {
+                path = $"D:\\git\\NoesisStudy\\Assets\\StreamingAssets\\{defaultImageName}";
+            }
 #if NOESIS
-            var fileData = File.ReadAllBytes($"D:\\git\\NoesisStudy\\Assets\\StreamingAssets\\{imageFileName}.jpg");
+            var fileData = File.ReadAllBytes(path);
             var tex = new Texture2D(2, 2);
             tex.LoadImage(fileData);
             return new ImageBrush(new TextureSource(tex));
 #else
-            var image = new BitmapImage(new Uri($"D:\\git\\NoesisStudy\\Assets\\StreamingAssets\\{imageFileName}.jpg"));
+            var image = new BitmapImage(new Uri(path));
             return new ImageBrush(image);
 #endif
         }
